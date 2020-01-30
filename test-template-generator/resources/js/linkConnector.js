3aeca72bc08f59aee6a0d4edc2d345afbdba6833
h7x4 <h7x4abk3g@protonmail.com>
2020-01-30 10:37:08 +0100

    Updated css

--- test-template-generator/resources/js/linkConnector.js
+++ test-template-generator/resources/js/linkConnector.js
@@ -2,9 +2,27 @@ const taskArray = tasks.tasks;
 const grid = document.getElementsByClassName("textboxGrid")[0];
 
 for (i=0; i<taskArray.length; i++) {
-    let mess = '<div class=linkElement>';
-    mess += '<h2><a href="' + taskArray[i]["path"] + '">' + taskArray[i]["name"] + '</a></h2>';
-    mess += '</div>';
 
-    grid.innerHTML+=mess;
+    const linkElement = document.createElement("div");
+    linkElement.className = "linkElement";
+
+    //TODO: taskArray needs to be preprocessed somehow
+    linkElement.addEventListener("click", function () {
+        window.location = taskArray[i]["path"];
+    } )
+
+    const h2 = document.createElement("h2");
+    const link = document.createElement("a");
+    link.href = taskArray[i]["path"];
+    link.innerHTML = taskArray[i]["name"];
+
+    h2.appendChild(link);
+    linkElement.appendChild(h2);
+    grid.appendChild(linkElement);
+
+    console.log(grid.querySelectorAll(".linkElement")[i]);
+    grid.querySelectorAll(".linkElement")[i].addEventListener("onclick", function () {
+
+        window.location = taskArray[i]["path"];
+    }, false);
 }
